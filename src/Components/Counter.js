02432cc3a13a76cf7964ac2083728b2afa115632
Saxena <saxenak@student.douglascollege.ca>
2024-04-18 17:34:23 -0700

    first commit

---
+++ src/Components/Counter.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Counter(props) {
+  return (
+    <div className="counter text-center">
+        <h2>{props.counter}</h2>
+
+    </div> 
+  )
+}
+
+export default Counter;
